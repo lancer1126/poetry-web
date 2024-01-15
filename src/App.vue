2c40c0b4005f8b2b1f98bada945b7193a8904601
lancer1126 <lancer1582@gmail.com>
2024-01-15 23:01:08 +0800

    主页添加背景图片

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="app">
-    <router-view />
+  <div class="background-image">
+    <div class="app">
+      <router-view />
+    </div>
   </div>
 </template>
 
@@ -13,5 +15,14 @@
   text-align: center;
   margin: auto;
   padding: 2rem;
+}
+
+.background-image {
+  background: url("public/宋-高克明-溪山瑞雪图全卷.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 100vh;
+  width: 100%;
 }
 </style>
